1207d2b6b39cd228ad20d2704d6342d128ea6268
FuseCPDevOPS <DevOPS@fusecp.com>
2026-04-10 23:03:18 +0200

    security: CodeQL remediation batch 41 - fix-null-deref-forgiving-span.ps1

--- FuseCP/Sources/FuseCP.Build/WebServices.cs
+++ FuseCP/Sources/FuseCP.Build/WebServices.cs
@@ -317,7 +317,7 @@ namespace FuseCP.Build
 				var clientAssemblyClass = ParseMemberDeclaration(
 					new ClientAssemblyClass()
 					{
-						OldNamespace = (((old)!N)!S)!.Name.ToString(),
+						OldNamespace = ((((ol)!d)!N)!S)!.Name.ToString(),
 						Class = ws.Class,
 						WebMethods = globalizedMethods
 					}
